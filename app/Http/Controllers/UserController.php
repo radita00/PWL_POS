ac11bc45c297337ab7c20d62c8bc7edb4c512804
Radita Yoga Anugrah <radita.yogaanugrah2@gmail.com>
2026-03-11 04:00:49 +0700

    Praktikum 1 - $fillable:

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,27 +10,15 @@ class UserController extends Controller
 {
     public function index()
     {
-        // $user = UserModel::all();
-        // return view('user', ['user' => $user]);
-        // Tambah data user dengan Eloquent Model
-        // $data = [
-        //     'username' => 'customer-1',
-        //     'nama' => 'Pelanggan',
-        //     'password' => Hash::make('12345'),
-        //     'level_id' => 4
-        // ];
-        // UserModel::insert($data); // tambahkan data ke tabel m_user
-
-        // Coba akses model UserModel
-        // $user = UserModel::all(); // ambil semua data dari tabel m_user
-        // return view('user', ['data' => $user]);
-        // tambah data user dengan Eloquent Model
         $data = [
-            'nama' => 'Pelanggan Pertama',
+            'level_id' => 2,
+            'username' => 'manager_tiga',
+            'nama' => 'Manager 3',
+            'password' => Hash::make('12345')
         ];
-        UserModel::where('username', 'customer-1')->update($data); // update data user
 
-        // coba akses model UserModel
+        UserModel::create($data);
+
         $user = UserModel::all(); // ambil semua data dari tabel m_user
         return view('user', ['data' => $user]);
     }
