ff81747f40b14d46e9bc38faaf5788abc4857017
Radita Yoga Anugrah <radita.yogaanugrah2@gmail.com>
2026-03-11 04:35:25 +0700

    Praktikum 2.3 – Retreiving Aggregrates

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -4,20 +4,16 @@
     <title>Data User</title>
 </head>
 <body>
-    <h1>Data User</h1>
-    <table border="1" cellpadding="2" cellspacing="0">
+        <h1>Data User</h1>
+
+    <table border="1">
         <tr>
-            <th>ID</th>
-            <th>Username</th>
-            <th>Nama</th>
-            <th>ID Level Pengguna</th>
+            <th>Jumlah Pengguna</th>
         </tr>
         <tr>
-            <td>{{ $data->user_id }}</td>
-            <td>{{ $data->username }}</td>
-            <td>{{ $data->nama }}</td>
-            <td>{{ $data->level_id }}</td>
+            <td>{{ $data }}</td>
         </tr>
     </table>
 </body>
+
 </html>
